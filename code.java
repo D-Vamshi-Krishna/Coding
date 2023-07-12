f67c9d9fc15e274314dd6852f9405e824e0e8a6e
D Vamshi Krishna <139313011+D-Vamshi-Krishna@users.noreply.github.com>
2023-07-13 01:27:19 +0530

    Update code.java
    Added Additional Statements

--- code.java
+++ code.java
@@ -9,23 +9,39 @@ public class Song {
     public void getInput() {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter Initial Capacity : ");
-        int n = input.nextInt();
-        System.out.println("Only "+n+" song/s can be fetched");
+        int cap = input.nextInt();
+        System.out.println("Not more than "+cap+" song/s can be fetched at a time in your Playlist");
         System.out.println("Enter Number of Songs : ");
-        int m = input.nextInt();
-        returnPlaylist(n,m);
+        int songCount = input.nextInt();
+        if (cap>=songCount) {
+            returnPlaylistWithSufficientCapacity(songCount);
+        }
+        else {
+            returnPlaylistWithInsufficientCapacity(songCount, cap);
+        }
     }
   
-    public void returnPlaylist(int a, int b) {
-        if(a>=b) {
-            String[] songArray = new String[b];
-            for (int i=0; i<b; i++) {
-                System.out.println("Enter Song No "+(i+1));
-                Scanner input = new Scanner(System.in);
-                String songName = input.nextLine();
-                songArray[i] = songName;
-            }
-            System.out.println("PLAYLIST : "+ java.util.Arrays.toString(songArray));
+    public void returnPlaylistWithSufficientCapacity(int a) {
+        System.out.println("DONT WORRY! YOUR SONGS COUNT IS WELL UNDER THE INITIAL CAPACITY");
+        String[] playListArray = createPlaylist(a);
+        System.out.println("PLAYLIST : "+ java.util.Arrays.toString(playListArray));
+        System.out.println("HAVE A GOOD TIME ENJOYING THE SONGS BUDDY "+"\uD83D\uDE00");
+    }
+    
+    public void returnPlaylistWithInsufficientCapacity(int a, int b) {
+        System.out.println("OOPS! YOUR SONGS COUNT IS EXCEEDING THE INITIAL CAPACITY.");
+        createPlaylist(b);
+    }
+    
+    public String[] createPlaylist(int x) {
+        String[] songArray = new String[x];
+        for (int i=0; i<x; i++) {
+            System.out.println("Enter Song No "+(i+1));
+            Scanner input = new Scanner(System.in);
+            String songName = input.nextLine();
+            songArray[i] = songName;
         }
-    }  
+        return songArray;
+    }
+
 }
